9168535eabf599f1be17542d63d3119d5ec76b4d
DenisSubbota <denis.subbota.v@gmail.com>
2023-08-10 13:03:17 +0500

    security group configuration

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,23 @@
 provider "aws" {
     
 }
+
+resource "aws_security_group" "k8s_security_group" {
+    name = "Dinamic Security group"
+
+    dynimic "ingress" {
+        for each ["80","443"]
+        content {
+            from_port = ingress.value
+            to_port   = ingress.value
+            protocol  = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
