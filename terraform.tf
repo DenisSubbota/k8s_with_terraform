90c131e23335dc027d06f0a2c16060d3589da0a6
DenisSubbota <denis.subbota.v@gmail.com>
2023-08-10 14:31:32 +0500

    security group to separate file

--- terraform.tf
+++ terraform.tf
@@ -15,28 +15,3 @@ resource "aws_instance" "denis_test_instance_terraform" {
   
 }
 
-resource "aws_security_group" "k8s_security_group" {
-    name = "Dinamic Security group"
-
-    dynamic "ingress" {
-        for_each = [ "22","6443" ]
-        content {
-            from_port = ingress.value
-            to_port   = ingress.value
-            protocol  = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-    }
-    egress {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = {
-        Name = "Denis terraform security group"
-        Owner = "Denis Subbota"
-        Project = "terraform+k8s configuration"
-    } 
-
-}
