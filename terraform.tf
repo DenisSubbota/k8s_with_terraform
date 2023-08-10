ed543b84baf34b925b0f6da281ee342a432f4f4e
DenisSubbota <denis.subbota.v@gmail.com>
2023-08-10 13:47:41 +0500

    profile added, and one test instance

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+    profile = "sso-aws-gs-sandbox-engineer-482433642182"
 }
 
-# Configure the AWS Provider
-provider "aws" {
- 
+resource "aws_instance" "denis_test_instance_terraform" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t3.micro"
+        
+        tags = {
+        name = "Denis terraform instance"
+        owner = "Denis Subbota"
+        project = "terraform+k8s configuration"
+    } 
+  
 }
 
 resource "aws_security_group" "k8s_security_group" {
